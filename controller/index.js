8e34edb8869a8ffa84c018778389043199955f9e
raghuvamsi <raghuvamsi.t@bajajfinserv.in>
2025-08-18 19:24:57 +0530

    fixes

--- controller/index.js
+++ controller/index.js
@@ -14,7 +14,7 @@ import { PassThrough } from 'stream';
 // const magnetLink = 'magnet:?xt=urn:btih:7969590D4697BDF90F680729D15560C9F95B160E';
 
 // Option 2: Big Buck Bunny (more peers available) - Default fallback
-const defaultMagnetLink = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com';
+const defaultMagnetLink = 'magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c'
 
 let currentMagnetLink = null; // Will be set by client
 
@@ -123,7 +123,8 @@ function startTorrentDownload(magnetLink = null) {
       name: file.name,
       size: file.length,
       totalFiles: videoFiles.length,
-      transcoding: requiresTranscoding
+      transcoding: requiresTranscoding,
+      codec: 'video/mp4; codecs="avc1.4d4028, mp4a.40.2"'
     };
     
     wss.clients.forEach(ws => {
